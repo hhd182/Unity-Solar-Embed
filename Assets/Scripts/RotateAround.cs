7d092ce54ac4b6d8d7acab6bb33612c6835f53bb
hhd <82596802+hhd182@users.noreply.github.com>
2024-05-26 20:01:33 +0700

    first commit

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed;
+
+    void Start()
+    {
+        if (target == null)
+        {
+            target = this.gameObject.transform;
+        }
+    }
+
+    void Update()
+    {
+        transform.RotateAround(target.transform.position, target.transform.up, speed * Time.deltaTime);
+    }
+}
